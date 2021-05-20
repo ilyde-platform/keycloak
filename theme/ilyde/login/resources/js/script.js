ab11cb66c8f6392f86ce3c0820a1d228327920b7
Tony Wilson <tony.wilson@hopenly.com>
2021-05-20 10:42:04 +0200

    Fixing css using javascript

--- theme/ilyde/login/resources/js/script.js
+++ theme/ilyde/login/resources/js/script.js
@@ -8,4 +8,13 @@ window.addEventListener('load', (event) => {
     ];
     var image = images[Math.floor(Math.random() * images.length)];
     bg.classList.add(image);
+    // add css classes
+    // first child class login-pf-page 
+    bg.firstElementChild.classList.add("login-pf-page");
+    // children of first child
+    var children = bg.firstElementChild.children;
+    children[1].classList.add("card-pf");
+    document.getElementById("kc-header").classList.add("login-pf-page-header");
+    document.getElementsByTagName("header")[0].classList.add("login-pf-header");
+    document.getElementById("kc-form-options").parentElement.classList.add("login-pf-settings");
 });
